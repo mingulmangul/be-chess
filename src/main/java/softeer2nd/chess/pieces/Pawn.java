df74de3e2749d013faecb97220fe4cd978ed3557
Kwon Min A <min2010011@gmail.com>
2023-07-04 16:22:13 +0900

    Pawn 색상별 출력 문자 부여

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -4,18 +4,27 @@ public class Pawn {
 
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
 
     private String color;
+    private char representation;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
     public Pawn(String color) {
         this.color = color;
+        this.representation = (color.equals(WHITE_COLOR)) ? WHITE_REPRESENTATION : BLACK_REPRESENTATION;
     }
 
     public String getColor() {
-        return color;
+        return this.color;
+    }
+
+    public char getRepresentation() {
+        return this.representation;
     }
 }
--- src/test/java/softeer2nd/chess/pieces/PawnTest.java
+++ src/test/java/softeer2nd/chess/pieces/PawnTest.java
@@ -8,22 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
+    }
+
     @Test
     @DisplayName("흰색과 검정색 폰이 생성되어야 한다")
     void create() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
-    }
-
-    void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
     @Test
-    @DisplayName("기본 생성자로 폰이 생성되어야 한다")
-    void create_기본생성자() {
+    @DisplayName("기본 생성자로 흰색 폰이 생성되어야 한다")
+    void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 }
