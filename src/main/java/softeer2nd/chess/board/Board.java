806e21d56d0dfb4c1ef2059ab4198da7d81d5496
Min A <min2010011@gmail.com>
2023-07-06 17:40:35 +0900

    feat: 체스판의 기물을 관리하는 기능 추가
    - 기물 개수 계산
    - 기물 조회
    - 기물 추가
    - 전체 기물의 점수 계산

--- src/main/java/softeer2nd/chess/board/Board.java
+++ src/main/java/softeer2nd/chess/board/Board.java
@@ -1,9 +1,12 @@
 package softeer2nd.chess.board;
 
+import softeer2nd.chess.pieces.Piece;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static softeer2nd.chess.pieces.Piece.Color;
+import static softeer2nd.chess.pieces.Piece.Type;
 
 public class Board {
 
@@ -50,4 +53,76 @@ public class Board {
         return sb.toString();
     }
 
+    /**
+     * 특정 색상과 종류를 가진 기물의 개수를 계산한다
+     *
+     * @param color 기물의 색상
+     * @param type  기물의 종류
+     * @return 기물의 개수
+     */
+    public int countPiecesWithColorAndType(Color color, Type type) {
+        int count = 0;
+        for (Rank rank : chessBoard) {
+            count += rank.countPiecesWithColorAndType(color, type);
+        }
+        return count;
+    }
+
+    /**
+     * 특정 위치의 기물을 조회한다
+     *
+     * @param position 위치
+     * @return 찾아 낸 기물
+     */
+    public Piece findPiece(String position) {
+        Position pos = Position.from(position);
+        return chessBoard.get(pos.getRankIndex()).getPieceAt(pos.getFileIndex());
+    }
+
+    /**
+     * 특정 위치에 기물을 추가한다
+     *
+     * @param position 위치
+     * @param piece    추가할 기물
+     */
+    public void setPiece(String position, Piece piece) {
+        Position pos = Position.from(position);
+        chessBoard.get(pos.getRankIndex())
+                .setPiece(pos.getFileIndex(), piece);
+    }
+
+    /**
+     * 체스판의 점수를 계산한다
+     *
+     * @param color 점수를 계산할 팀의 색상
+     * @return 점수
+     */
+    public double calculatePoint(Color color) {
+        double point = 0.0d;
+
+        for (int fileIndex = 0; fileIndex < SIZE; fileIndex++) {
+            double pawnPoint = 0.0d;
+
+            for (int rankIndex = 0; rankIndex < SIZE; rankIndex++) {
+                Piece piece = chessBoard.get(rankIndex).getPieceAt(fileIndex);
+
+                if (!piece.getColor().equals(color)) {
+                    continue;
+                }
+
+                if (piece.getType().equals(Type.PAWN)) {
+                    pawnPoint += piece.getPoint();
+                } else {
+                    point += piece.getPoint();
+                }
+            }
+
+            if (pawnPoint > 1) {
+                pawnPoint /= 2;
+            }
+            point += pawnPoint;
+        }
+
+        return point;
+    }
 }
