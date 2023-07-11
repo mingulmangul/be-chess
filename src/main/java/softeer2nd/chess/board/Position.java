bd36029ff54392452bb96e52ca7b3388277e7b3f
Kwon Min A <min2010011@gmail.com>
2023-07-11 16:34:41 +0900

    feat: 유효하지 않은 좌표에 대한 예외 처리 추가
    - 유효한 범위 내의 좌표가 아니면 OutOfBoardException 발생

--- src/main/java/softeer2nd/chess/board/Position.java
+++ src/main/java/softeer2nd/chess/board/Position.java
@@ -1,5 +1,7 @@
 package softeer2nd.chess.board;
 
+import softeer2nd.exceptions.OutOfBoardException;
+
 public class Position {
 
     private final int x;    // File
@@ -12,19 +14,22 @@ public class Position {
 
     // 팩토리 메소드
     public static Position of(String position) {
-        if (isNotValid(position)) {
-            throw new IllegalArgumentException("잘못된 좌표입니다");
-        }
         int x = position.charAt(0) - 'a';
         int y = position.charAt(1) - '1';
+        verifyRange(x, y);
+        return new Position(x, y);
+    }
+
+    public static Position of(int x, int y) {
+        verifyRange(x, y);
         return new Position(x, y);
     }
 
     // 좌표가 유효한 범위 내에 있는지 검증한다
-    public static boolean isNotValid(String position) {
-        char file = position.charAt(0);
-        char rank = position.charAt(1);
-        return 'a' > file || file > 'h' || '1' > rank || rank > '8';
+    private static void verifyRange(int x, int y) {
+        if ((0 > x) || (x >= 8) || (0 > y) || (y >= 8)) {
+            throw new OutOfBoardException();
+        }
     }
 
     // x(file)를 반환한다
