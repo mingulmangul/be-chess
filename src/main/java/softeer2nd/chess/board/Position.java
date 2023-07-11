d6167702916b2bb30638cdc8a3537fc70d080ad2
Kwon Min A <min2010011@gmail.com>
2023-07-11 11:07:10 +0900

    style(Position): 검증 메소드 반전
    - isValid() -> isNotValid()

--- src/main/java/softeer2nd/chess/board/Position.java
+++ src/main/java/softeer2nd/chess/board/Position.java
@@ -12,7 +12,7 @@ public class Position {
 
     // 팩토리 메소드
     public static Position of(String position) {
-        if (!isValid(position)) {
+        if (isNotValid(position)) {
             throw new IllegalArgumentException("잘못된 좌표입니다");
         }
         int x = position.charAt(0) - 'a';
@@ -21,10 +21,10 @@ public class Position {
     }
 
     // 좌표가 유효한 범위 내에 있는지 검증한다
-    public static boolean isValid(String position) {
+    public static boolean isNotValid(String position) {
         char file = position.charAt(0);
         char rank = position.charAt(1);
-        return 'a' <= file && file <= 'h' && '1' <= rank && rank <= '8';
+        return 'a' > file || file > 'h' || '1' > rank || rank > '8';
     }
 
     // x(file)를 반환한다
