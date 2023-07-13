168831667eaeab40c1a4764a0df617673b446029
Kwon Min A <min2010011@gmail.com>
2023-07-13 16:37:24 +0900

    refactor: Chess에서 사용하는 상수를 정적 내부 클래스로 관리

--- src/main/java/softeer2nd/chess/Chess.java
+++ src/main/java/softeer2nd/chess/Chess.java
@@ -9,15 +9,6 @@ import java.util.Scanner;
 
 public class Chess {
 
-    private static final int MAX_TURN = 100;
-    private static final String START_COMMAND = "start";
-    private static final String END_COMMAND = "end";
-    private static final String MOVE_COMMAND = "move";
-    private static final int MOVE_COMMAND_LENGTH = 3;
-    private static final int MOVE_COMMAND_INDEX = 0;
-    private static final int SOURCE_INDEX = 1;
-    private static final int TARGET_INDEX = 2;
-
     private final ChessGame chessGame;
     private final ChessView chessView;
     private Scanner sc;
@@ -40,7 +31,7 @@ public class Chess {
                 // 명령어 검증
                 verifyProgramCommand(command);
                 // 프로그램 종료
-                if (command.equals(END_COMMAND)) {
+                if (command.equals(Constant.END_COMMAND)) {
                     break;
                 }
                 // 새로운 체스 게임 시작
@@ -58,13 +49,13 @@ public class Chess {
     // 게임은 한 팀의 킹이 잡히거나 MAX_TURN(100) 만큼 턴을 반복할 때까지 진행한다
     private void playGame() {
         int turn = 0;
-        while (turn < MAX_TURN) {
+        while (turn < Constant.MAX_TURN) {
             chessView.printTurnStartMessage(chessGame.getCurrentTurn());
 
             String command = sc.nextLine().toLowerCase();
             try {
                 String[] commands = separateMoveCommand(command);
-                chessGame.move(commands[SOURCE_INDEX], commands[TARGET_INDEX]);
+                chessGame.move(commands[Constant.SOURCE_INDEX], commands[Constant.TARGET_INDEX]);
                 turn++;
             } catch (ChessGameException e) {
                 chessView.printErrorMessage(e.getMessage());
@@ -74,18 +65,33 @@ public class Chess {
 
     // 게임 시작과 프로그램 종료 명령어를 검증한다
     private void verifyProgramCommand(String command) {
-        if (!command.equals(START_COMMAND) && !command.equals(END_COMMAND)) {
+        if (!command.equals(Constant.START_COMMAND) && !command.equals(Constant.END_COMMAND)) {
             throw new InvalidCommandException();
         }
     }
 
     // 이동 명령어를 검증한다
     private String[] separateMoveCommand(String command) {
-        String[] commands = command.split(" ");
-        if (commands.length != MOVE_COMMAND_LENGTH ||
-                !commands[MOVE_COMMAND_INDEX].equals(MOVE_COMMAND)) {
+        String[] commands = command.split(Constant.SEPARATOR);
+        if (commands.length != Constant.MOVE_COMMAND_LENGTH ||
+                !commands[Constant.MOVE_COMMAND_INDEX].equals(Constant.MOVE_COMMAND)) {
             throw new InvalidCommandException();
         }
         return commands;
+    }
+
+    // Chess 클래스에서 사용하는 상수를 저장하는 정적 내부 클래스
+    private static class Constant {
+
+        private static final int MAX_TURN = 100;
+        private static final String START_COMMAND = "start";
+        private static final String END_COMMAND = "end";
+        private static final String MOVE_COMMAND = "move";
+        private static final String SEPARATOR = " ";
+        private static final int MOVE_COMMAND_LENGTH = 3;
+        private static final int MOVE_COMMAND_INDEX = 0;
+        private static final int SOURCE_INDEX = 1;
+        private static final int TARGET_INDEX = 2;
+
     }
 }
